35b859e5691bf2a24dbbefeaa119dd2fec2f328c
turcioskevin <kevinturc@icloud.com>
2026-04-02 20:07:13 -0400

    Update and rename CatacombCrawler.java to Main.java

--- project-03-catacombCrawler/Main.java
+++ project-03-catacombCrawler/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class CatacombCrawler {
+public class Main {
     public static void main(String[] args) {
 
         Scanner scnr = new Scanner(System.in);
